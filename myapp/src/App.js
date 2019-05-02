9c3e5277ce1bd5db57d93165d5f08c9cb77f1b3d
romeliyan <rkbliyanage@gmail.com>
2019-05-02 17:00:03 +0530

    tutorial 13

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -6,7 +6,11 @@ function App() {
     <div className="App">
       <h1>My First React App</h1>
       <p>Hello World !</p>
-      <Ninjas />
+      <Ninjas name="Roma" age="35" belt="green" />
+
+      <Ninjas name="Kamal" age="45" belt="brown" />
+
+      <Ninjas name="Sunil" age="50" belt="orange" />
     </div>
   );
 }
