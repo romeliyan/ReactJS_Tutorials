a5a5ce3a949a3babed89e4f55e610be4981591b4
romeliyan <rkbliyanage@gmail.com>
2019-05-02 16:38:57 +0530

    tutorial 12

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
+import Ninjas from "./Ninjas";
 
 function App() {
   return (
     <div className="App">
       <h1>My First React App</h1>
       <p>Hello World !</p>
+      <Ninjas />
     </div>
   );
 }
