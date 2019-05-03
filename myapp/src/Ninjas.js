5b3116c654b12db2be1bd0b00b44be0f88ee61d3
romeliyan <rkbliyanage@gmail.com>
2019-05-03 11:27:47 +0530

    tutorial 16

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -2,13 +2,17 @@ import React from "react";
 
 const Ninjas = ({ ninjas }) => {
   const ninjaList = ninjas.map(ninja => {
-    return (
-      <div className="ninja" key={ninja.id}>
-        <div> Name : {ninja.name}</div>
-        <div> Age : {ninja.age}</div>
-        <div> Belt : {ninja.belt}</div>
-      </div>
-    );
+    if (ninja.age > 50) {
+      return (
+        <div className="ninja" key={ninja.id}>
+          <div> Name : {ninja.name}</div>
+          <div> Age : {ninja.age}</div>
+          <div> Belt : {ninja.belt}</div>
+        </div>
+      );
+    } else {
+      return null;
+    }
   });
   return <div className="ninja-list">{ninjaList}</div>;
 };
